a6c60b63d26a5fec9e8f7260dbb86bfd6b533007
Krishna Mohan <krishanmohank974@gmail.com>
2024-03-21 06:32:29 +0530

    ...

--- src/portfolio.js
+++ src/portfolio.js
@@ -314,7 +314,7 @@ const degrees = {
       descriptions: [
         "⚡ I have studied basic software engineering subjects like DS, Algorithms, DBMS, OS, CA, AI etc.",
         "⚡ Apart from this, I have done courses on Deep Learning, Data Science, Cloud Computing and Full Stack Development.",
-        "⚡ I was selected for Merit cum Means Scholarship which is given to top 10% of students in college. I have received award from respected director for consistently best performance in academics.",
+        ,
       ],
       website_link: "",
     },
